da5ab1f0cc0ebf9c49410f2c2bb55908ccfaef01
IbraheemLanre <s2001108@edu.bc.fi>
2021-08-05 22:26:39 +0300

    random access file in java.nio

--- JavaNIO/src/com/ibraheem/Main.java
+++ JavaNIO/src/com/ibraheem/Main.java
@@ -1,7 +1,11 @@
 package com.ibraheem;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,15 +14,34 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        try{
-            //FileInputStream file = new FileInputStream("data.txt");
-            //FileChannel channel = file.getChannel();
-            Path dataPath = FileSystems.getDefault().getPath("data.txt");
-            Files.write(dataPath, "\nLine 5".getBytes("UTF-8"), StandardOpenOption.APPEND);
-            List<String> lines = Files.readAllLines(dataPath);
-            for(String line : lines){
-                System.out.println(line);
-            }
+        try(FileOutputStream binFile = new FileOutputStream("data.dat");
+            FileChannel binChannel = binFile.getChannel()){
+            byte[] outputBytes = "Hello World!".getBytes();
+            ByteBuffer buffer = ByteBuffer.wrap(outputBytes);
+            int numBytes = binChannel.write(buffer);
+            System.out.println("numBytes written was: " + numBytes);
+
+            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+            intBuffer.putInt(245);
+            intBuffer.flip();
+            numBytes = binChannel.write(intBuffer);
+            System.out.println("numBytes written was: " + numBytes);
+
+            intBuffer.flip();
+            intBuffer.putInt(-98765);
+            intBuffer.flip();
+            numBytes = binChannel.write(intBuffer);
+            System.out.println("numBytes written was: " + numBytes);
+
+            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+            byte[] b = new byte[outputBytes.length];
+            ra.read(b);
+            System.out.println(new String(b));
+
+            long int1 = ra.readInt();
+            long int2 = ra.readInt();
+            System.out.println(int1);
+            System.out.println(int2);
         }catch (IOException e){
             e.printStackTrace();
         }
