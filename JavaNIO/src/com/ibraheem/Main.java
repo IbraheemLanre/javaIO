08f16e8f3e9fb709bf5899bb9000a3c8aea6e3e5
IbraheemLanre <s2001108@edu.bc.fi>
2021-08-23 01:09:57 +0300

    reading and writing data using JAVANIO Buffere

--- JavaNIO/src/com/ibraheem/Main.java
+++ JavaNIO/src/com/ibraheem/Main.java
@@ -16,32 +16,36 @@ public class Main {
     public static void main(String[] args) {
         try(FileOutputStream binFile = new FileOutputStream("data.dat");
             FileChannel binChannel = binFile.getChannel()){
+
+            // to write to a local file
+            ByteBuffer buffer = ByteBuffer.allocate(100);
             byte[] outputBytes = "Hello World!".getBytes();
-            ByteBuffer buffer = ByteBuffer.wrap(outputBytes);
-            int numBytes = binChannel.write(buffer);
-            System.out.println("numBytes written was: " + numBytes);
+            buffer.put(outputBytes);
+            buffer.putInt(245);
+            buffer.putInt(-98765);
+            byte [] outputBytes2 = "Nice to meet you".getBytes();
+            buffer.put(outputBytes2);
+            buffer.putInt(1000);
+            buffer.flip();
 
-            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
-            intBuffer.putInt(245);
-            intBuffer.flip();
-            numBytes = binChannel.write(intBuffer);
-            System.out.println("numBytes written was: " + numBytes);
+            binChannel.write(buffer);
 
-            intBuffer.flip();
-            intBuffer.putInt(-98765);
-            intBuffer.flip();
-            numBytes = binChannel.write(intBuffer);
-            System.out.println("numBytes written was: " + numBytes);
+            // To read buffer from a local file
+            RandomAccessFile ra = new RandomAccessFile("data.dat","rwd");
+            FileChannel channel = ra.getChannel();
 
-            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
-            byte[] b = new byte[outputBytes.length];
-            ra.read(b);
-            System.out.println(new String(b));
-
-            long int1 = ra.readInt();
-            long int2 = ra.readInt();
-            System.out.println(int1);
-            System.out.println(int2);
+            ByteBuffer readBuffer = ByteBuffer.allocate(100);
+            channel.read(readBuffer);
+            readBuffer.flip();
+            byte [] inputString = new byte[outputBytes.length];
+            readBuffer.get(inputString);
+            System.out.println("inputString = " + new String(inputString));
+            System.out.println("int1 = " + readBuffer.getInt());
+            System.out.println("int2 = " + readBuffer.getInt());
+            byte [] inputString2 = new byte[outputBytes2.length];
+            readBuffer.get(inputString2);
+            System.out.println("inputString2 = " + new String(inputString2));
+            System.out.println("int3 = " + readBuffer.getInt());
         }catch (IOException e){
             e.printStackTrace();
         }
